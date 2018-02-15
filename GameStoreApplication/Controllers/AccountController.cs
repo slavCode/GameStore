9bf3911d8146110fae28ff3273cd0d4e46280879
Slav Tomov <stomow@gmail.com>
2018-02-15 19:46:46 +0200

    Create controller and html

---
+++ GameStoreApplication/Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+﻿namespace GameStoreApplication.Controllers
+{
+    using Server.Http.Contracts;
+
+    public class AccountController : Controller
+    {
+        public IHttpResponse Register()
+        {
+            return this.FileViewResponse(@"account\register");
+        }
+    }
+}
