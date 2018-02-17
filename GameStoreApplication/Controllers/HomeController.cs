4c600cf7878ed4f5f3ab4a790756425e3bceaff3
Slav Tomov <stomow@gmail.com>
2018-02-18 00:30:40 +0200

    Implement home page

---
+++ GameStoreApplication/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿namespace GameStoreApplication.Controllers
+{
+    using Server.Http.Contracts;
+
+    public class HomeController : Controller
+    {
+        public HomeController(IHttpRequest request) 
+            : base(request)
+        {
+        }
+
+        public IHttpResponse Index()
+        {
+            return this.FileViewResponse(@"home\index");
+        }
+    }
+}
