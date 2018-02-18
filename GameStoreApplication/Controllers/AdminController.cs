d9f6259190414d25536b60d8750b64f8cedc87f5
Slav Tomov <stomow@gmail.com>
2018-02-18 11:59:14 +0200

    Implement add-games

---
+++ GameStoreApplication/Controllers/AdminController.cs
@@ -0,0 +1,19 @@
+﻿namespace GameStoreApplication.Controllers
+{
+    using Server.Http.Contracts;
+
+    public class AdminController : Controller
+    {
+        private const string AddGamePath = @"admin\add-game";
+
+        public AdminController(IHttpRequest request) 
+            : base(request)
+        {
+        }
+
+        public IHttpResponse Add()
+        {
+            return this.FileViewResponse(AddGamePath);
+        }
+    }
+}
