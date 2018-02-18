b8bb1832c577e72a8bb1cd1a15da6bfd86863001
Slav Tomov <stomow@gmail.com>
2018-02-18 17:15:37 +0200

    Implement Game0list

---
+++ GameStoreApplication/ViewModels/Admin/AdminListGameViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace GameStoreApplication.ViewModels.Admin
+{
+    public class AdminListGameViewModel
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public decimal Price { get; set; }
+
+        public double Size { get; set; }
+
+    }
+}
